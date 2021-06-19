66d0437137f5743dcf8e034bb55747646499ab70
obrin <jordanyong23@gmail.com>
2021-06-19 15:26:03 +0200

    add env

--- zsh/index.zsh
+++ zsh/index.zsh
@@ -9,7 +9,7 @@ ZSH_THEME="powerlevel10k/powerlevel10k"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(fzf zsh-nvm zsh-z zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(fzf zsh-nvm zsh-z zsh-autosuggestions zsh-syntax-highlighting pyenv jenv)
 
 # Python
-eval $(pyenv init --path)
+#eval $(pyenv init --path)
