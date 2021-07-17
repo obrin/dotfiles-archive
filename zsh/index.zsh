c442b47a34c9e6abadd23c96d3173f6efa6f82c9
obrin <jordanyong23@gmail.com>
2021-07-17 17:38:12 +0200

    Change theme

--- zsh/index.zsh
+++ zsh/index.zsh
@@ -2,8 +2,8 @@
 # Look in ~/.oh-my-zsh/themes/
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
-# ZSH_THEME="robbyrussell"
-ZSH_THEME="powerlevel10k/powerlevel10k"
+ZSH_THEME="robbyrussell"
+# ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
