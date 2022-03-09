6451cd66c1fc58845fbbdd0e1f156e5bacca267b
obrin <jordanyong23@gmail.com>
2022-03-09 17:08:06 +0800

    Stop dropbox installation

--- dropbox/install.sh
+++ dropbox/install.sh
@@ -1,5 +1,5 @@
-export DROPBOX=$HOME/.dropbox-dist/dropboxd
+# export DROPBOX=$HOME/.dropbox-dist/dropboxd
 
-if [ ! -f $DROPBOX ]; then
-  sh -c "$(cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -)"
-fi
+# if [ ! -f $DROPBOX ]; then
+#   sh -c "$(cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -)"
+# fi
